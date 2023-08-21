1b78c1e1c0662c42b8c5b49cd0d8bef1ec968150
UgrasK <ugurkaraer1989@gmail.com>
2023-08-21 13:52:57 +0300

    Add project files.

---
+++ TryCatchFinallyDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinallyDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("you entered " + number);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: " + ex.Message.ToString());
+            }
+
+            finally
+            {
+                Console.WriteLine("successful");
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
